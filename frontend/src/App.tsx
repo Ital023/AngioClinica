e4b84bdfb8489603e33e078a74cd74d74b374e33
Ital023 <contadoitalo911@gmail.com>
2024-09-01 19:07:00 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import "./index.css"
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <Button className="bg-red-600 hover:bg-red-700">Button</Button>
+  )
+}
+
+
