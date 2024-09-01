60edece25b5d819105c8c13b1006d9f6ff8b2286
Ital023 <contadoitalo911@gmail.com>
2024-09-01 19:28:20 -0300

    feat: Libs shadcn ui, font awesome, React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import "./index.css"
-import { Button } from './components/ui/button'
 
 export default function App() {
   return (
-    <Button className="bg-red-600 hover:bg-red-700">Button</Button>
+    <h1>oi</h1>
   )
 }
 
